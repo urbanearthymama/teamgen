90b1e35e40718ff5b4d9526e61a4c1ba18841b4e
Patricia Nelson <urbanearthymama@gmail.com>
2021-10-16 21:20:28 -0500

    inquirer update

--- index.js
+++ index.js
@@ -2,42 +2,22 @@ const inquirer = require("inquirer");
 const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 
-const fs = require('fs');
-const { profile } = require("console");
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+// console.log(name, github);
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-const generatePage = (name, githubName) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
-
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-    `;
-};
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
 
 function entryPoint() {
   inquirer
     .prompt([
-      /* Pass your questions in here */
       {
         type: "input",
         name: "name",
